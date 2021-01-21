b236d16b2f683cb24ada5ec5c911b7cca89f5338
TechioRafael <techiorafael@gmail.com>
2021-01-21 20:35:55 -0300

    escaping the table's name and identifier column's name in constructor.  getRowsPerColumn function

--- backend/src/database/TableModel.js
+++ backend/src/database/TableModel.js
@@ -2,21 +2,21 @@ const connection = require('./connection');
 
 class TableModel {
     constructor(tableName, identifierColumnName, identifierColumnValue) {
-        this.tableName = tableName,
-            this.identifierColumnName = identifierColumnName,
-            this.identifierColumnValue = identifierColumnValue;
+        this.tableName = connection.escapeId(tableName);
+        this.identifierColumnName = connection.escapeId(identifierColumnName);
+        this.identifierColumnValue = identifierColumnValue;
     }
 
     async delete() {
         return await connection.sqlUpdate(`
-            DELETE FROM ${connection.escapeId(this.tableName)}
-            WHERE ${connection.escapeId(this.identifierColumnName)} = :rowid 
+            DELETE FROM ${this.tableName}
+            WHERE ${this.identifierColumnName} = :rowid 
         `, { rowid: this.identifierColumnValue, value: value })
     }
 
     async create(objectData) {
         return await connection.sqlInsert(`
-            INSERT INTO ${connection.escapeId(this.tableName)}
+            INSERT INTO ${this.tableName}
                 (${Object.keys(objectData).map(key => connection.escapeId(key)).join(", ")})
             VALUES (${Object.keys(objectData).map(key => `:${key}`).join(", ")})
         `, objectData);
@@ -24,38 +24,46 @@ class TableModel {
 
     async changeInfo(column, value) {
         return await connection.sqlUpdate(`
-            UPDATE ${connection.escapeId(this.identifierColumnName)} 
+            UPDATE ${this.identifierColumnName} 
             SET ${connection.escapeId(column)} = :value
-            FROM ${connection.escapeId(this.tableName)}
-            WHERE ${connection.escapeId(this.identifierColumnName)} = :rowid 
+            FROM ${this.tableName}
+            WHERE ${this.identifierColumnName} = :rowid 
         `, { rowid: this.identifierColumnValue, value: value })
     }
 
     async changeData(objectData) {
         return await connection.sqlUpdate(`
-            UPDATE ${connection.escapeId(this.identifierColumnName)}
+            UPDATE ${this.identifierColumnName}
             SET ${Object.keys(objectData).map(key => {
             return `${connection.escapeId(key)} = :${key}`
         })}
-            FROM ${connection.escapeId(this.tableName)}
-            WHERE ${connection.escapeId(this.identifierColumnName)} = :rowid
+            FROM ${this.tableName}
+            WHERE ${this.identifierColumnName} = :rowid
         `, { rowid: this.identifierColumnValue, ...objectData })
     }
 
     async getInfo(column) {
         return await connection.sqlSelectValue(`
             SELECT ${connection.escapeId(column)}
-            FROM ${connection.escapeId(this.tableName)}
-            WHERE ${connection.escapeId(this.identifierColumnName)} = :rowid 
+            FROM ${this.tableName}
+            WHERE ${this.identifierColumnName} = :rowid 
         `, { rowid: this.identifierColumnValue })
     }
 
     async getData(columns = ["*"]) {
         return await connection.sqlSelectRow(`
             SELECT ${columns.map(column => column == '*' ? column : connection.escapeId(column)).join(", ")}
-            FROM ${connection.escapeId(this.tableName)}
-            WHERE ${connection.escapeId(this.identifierColumnName)} = :rowid 
+            FROM ${this.tableName}
+            WHERE ${this.identifierColumnName} = :rowid 
         `, { rowid: this.identifierColumnValue })
+    }
+
+    async getRowsPerColumn(column, value) {
+        return await connection.sqlSelect(`
+            SELECT * 
+            FROM ${this.tableName}
+            WHERE ${connection.escapeId(column)} = :value
+        `, { value: value })
     }
 
     // Connection functions 
